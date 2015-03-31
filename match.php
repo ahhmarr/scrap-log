933a83c27f4f8d8a95010689069c84c8e1e7d8bf
ahmar siddiqui <ahmar.siddiqui@gmail.com>
2015-04-01 01:36:58 +0530

    made backup log entried too

--- match.php
+++ match.php
@@ -2,56 +2,59 @@
 require 'vendor/autoload.php';
 use GeoIp2\Database\Reader;
 $reader = new Reader(getcwd().'/GeoLite2-City.mmdb');
-// echo getcwd();
 //read file
-// $mainPath=getcwd()."/string.txt";
-$mainPath="/home/dsstudios/logs/frontend/access_Adification.log";
-// $logPath="test.txt";
-$logPath="/home/dsstudios/webapps/htdocs/plist.log";
-$file=fopen($mainPath,"r");
-$log=fopen($logPath,"w");
-
-while(($row=fgets($file))!==false)
+foreach(["",".1",".2",".3",".4",".5",".6",".7"] as $value)
 {
-//parse for content
-	// echo $row."\n";
-	$ip=$date=$file_name=$location="";
-	preg_match('/^[0-9.]*/',$row,$match);
-	$ip=(isset($match[0]))?$match[0]:"";
-	preg_match('/\[.*\]/', $row,$match);
-	$date=(isset($match[0]))?$match[0]:"";
-	if($date)
-	{
-		$date=str_replace("+0000", "", $date);
-		$date=str_replace("[", "", $date);
-		$date=str_replace("]", "", $date);
-	}
-	preg_match('/"GET \/.*.plist HTTP\/1.1" 200/',$row,$match);
-	$file_name=(isset($match[0]))?$match[0]:"";
-	if($file_name)
-	{
-		$file_name=str_replace('"GET /',"",$file_name);
-		$file_name=str_replace(' HTTP/1.1" 200',"",$file_name);
-	}
-	if(!$ip || !$date || !$file_name || substr_count($ip, ".")<3)
-		continue;
-	try
-	{
-		$city=$reader->city($ip);
-		$cityName=$city->city->name?",".$city->city->name:"";
-		$location=$city->country->name.$cityName;	
-	}
-	catch(Exception $e)
-	{
-		$location="Not Found";
-	}
-	$row=str_pad($date,25).
-		 str_pad($ip, 25).
-		 str_pad($file_name, 50).
-		 str_pad($location,15)."\n";
-	fwrite($log, $row);
+	writeLogEntries("/home/dsstudios/logs/frontend/access_Adification.log$value",
+					"/home/dsstudios/webapps/htdocs/plist.log.$value");
 }
-fclose($log);
-fclose($file);
+function writeLogEntries($mainPath,$logPath)
+{
+	$file=fopen($mainPath,"r");
+	$log=fopen($logPath,"w");
+
+	while(($row=fgets($file))!==false)
+	{
+	//parse for content
+		// echo $row."\n";
+		$ip=$date=$file_name=$location="";
+		preg_match('/^[0-9.]*/',$row,$match);
+		$ip=(isset($match[0]))?$match[0]:"";
+		preg_match('/\[.*\]/', $row,$match);
+		$date=(isset($match[0]))?$match[0]:"";
+		if($date)
+		{
+			$date=str_replace("+0000", "", $date);
+			$date=str_replace("[", "", $date);
+			$date=str_replace("]", "", $date);
+		}
+		preg_match('/"GET \/.*.plist HTTP\/1.1" 200/',$row,$match);
+		$file_name=(isset($match[0]))?$match[0]:"";
+		if($file_name)
+		{
+			$file_name=str_replace('"GET /',"",$file_name);
+			$file_name=str_replace(' HTTP/1.1" 200',"",$file_name);
+		}
+		if(!$ip || !$date || !$file_name || substr_count($ip, ".")<3)
+			continue;
+		try
+		{
+			$city=$reader->city($ip);
+			$cityName=$city->city->name?",".$city->city->name:"";
+			$location=$city->country->name.$cityName;	
+		}
+		catch(Exception $e)
+		{
+			$location="Not Found";
+		}
+		$row=str_pad($date,25).
+			 str_pad($ip, 25).
+			 str_pad($file_name, 50).
+			 str_pad($location,15)."\n";
+		fwrite($log, $row);
+	}
+	fclose($log);
+	fclose($file);
+}
 
 //print relevant content
